be56b8919ead789b9df1f33e5c3ceb627a63db90
Giles Chang <gileschang97@hotmail.com>
2020-12-04 22:12:54 +0800

    Added comments for readabiltiy for future updates

--- src/FacialDetection.java
+++ src/FacialDetection.java
@@ -18,12 +18,18 @@ public class FacialDetection {
 		String xmlFile = "xml/lbpcascade_frontalface.xml";
 		CascadeClassifier cc = new CascadeClassifier(xmlFile);
 		
-		MatOfRect faceDetection = new MatOfRect();
+		//We do this by first applying one cascade classifier scan over the the entire image 
+		//to give us an MatOfRect object containing our large object
+		MatOfRect faceDetection = new MatOfRect(); 
 		
-		//using classifier to reading src file
+		//using classifier to read src file and MatOfRect Object.
 		cc.detectMultiScale(src, faceDetection);
-		System.out.println(String.format("Face Found: %d", faceDetection.toArray().length));
-	
+		System.out.println(String.format("Face Found: %d", faceDetection.toArray().length)); //.toArray detects all the faces
+		/*
+		 * We then iterate over the Rect[] array given by the toArray() function from the MatOfRect object. 
+		 * This Rect object is used in creating a temporary Mat object that is "cropped" to the Rect object's properties 
+		 * (x, y, width, height) from the original image, where we can then perform detections on the temporary Mat object. 
+		 */
 		for(Rect rect: faceDetection.toArray()) {
 			Imgproc.rectangle(src, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0,0,255),3);
 		}
