271266356f9183e6ee72a3b96fc77f4a14d3d422
Giles Chang <gileschang97@hotmail.com>
2020-12-04 21:59:18 +0800

    Testing Image dataset with Classifier

--- src/FacialDetection.java
+++ src/FacialDetection.java
@@ -1,9 +1,34 @@
 import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 
 public class FacialDetection {
 	public static void main(String[] args) {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		System.out.println("cchec");
+//		System.out.println("cchec");
+		String imgFile = "images/test.jpg";
+		Mat src = Imgcodecs.imread(imgFile);
 		
+		String xmlFile = "xml/lbpcascade_frontalface.xml";
+		CascadeClassifier cc = new CascadeClassifier(xmlFile);
+		
+		MatOfRect faceDetection = new MatOfRect();
+		
+		//using classifier to reading src file
+		cc.detectMultiScale(src, faceDetection);
+		System.out.println(String.format("Face Found: %d", faceDetection.toArray().length));
+	
+		for(Rect rect: faceDetection.toArray()) {
+			Imgproc.rectangle(src, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0,0,255),3);
+		}
+	
+		Imgcodecs.imwrite("images/test-out.jpg", src);
+		System.out.println("Image Detected Successfully!");
 	}
 }
