70a125cb3f9582058fddf2d94a04e864c1908b5b
Giles Chang <gileschang97@hotmail.com>
2020-12-04 21:17:17 +0800

    Adding library and reading basic image

---
+++ src/FacialDetection.java
@@ -0,0 +1,9 @@
+import org.opencv.core.Core;
+
+public class FacialDetection {
+	public static void main(String[] args) {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		System.out.println("cchec");
+		
+	}
+}
